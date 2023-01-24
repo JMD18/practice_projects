599ea2d0bb3d5369224b0d883f52fcbd22969355
Josh <joshd18@uab.edu>
2023-01-23 21:58:16 -0600

    testing push

---
+++ Store_Project.java
@@ -0,0 +1,58 @@
+package practice_projects;
+
+import java.util.*;
+
+
+class Store {
+
+	ArrayList<Integer> values = new ArrayList<Integer>();
+    HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+    public static void main(String[] args) {
+        
+
+		Store s = new Store();
+
+		s.insert(s, 3);
+		s.insert(s, 4);
+		s.insert(s, 4);
+		s.insert(s, 5);
+
+		s.remove(s, 7);
+
+		s.getRandom(s);
+        
+    }
+
+
+
+	public void insert(Store s, int value) {
+		if(values.contains(value)) return;
+
+		s.values.add(value);
+		s.map.put(value,s.values.size() - 1);
+
+		System.out.println("\nAdded " + value + " to the map.");
+		System.out.println("Values: " + s.values.toString());
+		System.out.println("Map: " + s.map.toString());
+
+	}
+
+	public void remove(Store s, int value) {
+		if(!(values.contains(value))) {
+			System.out.println("\n" + value + " not found in values.");
+			// System.out.println("Values: " + s.values.toString());
+			// System.out.println("Map: " + s.map.toString());
+
+
+			return;
+		}
+
+	}
+
+	public void getRandom(Store s) {
+
+	}
+
+
+}
