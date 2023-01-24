88b10bdf0f6e6c8d89fb2e8c3b2d90ea3e21aa63
Josh Daugherty <joshmd00@gmail.com>
2023-01-24 15:06:09 -0600

    editing remove method (swapping)

--- Store_Project.java
+++ Store_Project.java
@@ -25,7 +25,7 @@ class Store {
 		s.insert(s, 4);
 		s.insert(s, 5);
 
-		s.remove(s, 7);
+		s.remove(s, 3);
 
 		s.getRandom(s);
         
@@ -34,32 +34,50 @@ class Store {
 
 
 	public void insert(Store s, int value) {
+
 		if(values.contains(value)) return;
 
 		s.values.add(value);
 		s.map.put(value,s.values.size() - 1);
 
-		// System.out.println("\nAdded " + value + " to the map.");
+
+		System.out.println("\nAdded " + value + " to the map.");
+		printValues(s);
+
+		System.out.println("Map: " + s.map.toString());
+
+	}
+
+	public void remove(Store s, int value) {
+
+		if(!(values.contains(value))) {
+			System.out.println("\n" + value + " not found in values.");
+			return;
+		}
+
+		// swapping element to remove to last place in list in order to pop
+		int index = s.map.get(value);
+		int temp_val = s.values.get(s.values.size()-1);
+
+
+
+		// System.out.println(index + " " + temp_val);
+		// System.out.println("Map: " + s.map.toString());		
+
+
+
 		// System.out.println("Values: " + s.values.toString());
 		// System.out.println("Map: " + s.map.toString());
 
 	}
 
-	public void remove(Store s, int value) {
-		if(!(values.contains(value))) {
-			System.out.println("\n" + value + " not found in values.");
-			// System.out.println("Values: " + s.values.toString());
-			// System.out.println("Map: " + s.map.toString());
-
-
-			return;
-		}
-
-	}
-
 	public void getRandom(Store s) {
-
+		
 	}
 
+
+	public void printValues(Store s) {
+		System.out.println(s.values.toString());
+	}	
 
 }
