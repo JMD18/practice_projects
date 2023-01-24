7000bc5e27bcdaff2c7e0520c7f9ac13c56cbfb1
Josh Daugherty <joshmd00@gmail.com>
2023-01-24 11:15:28 -0600

    added comment to describe project basis

--- Store_Project.java
+++ Store_Project.java
@@ -3,6 +3,13 @@ package practice_projects;
 import java.util.*;
 
 
+/*	Practice creating a class to go along with
+ * 	YT coding interview found at the link below:
+ * 
+ * 	https://youtu.be/46dZH7LDbf8
+ * 
+ */
+
 class Store {
 
 	ArrayList<Integer> values = new ArrayList<Integer>();
@@ -32,9 +39,9 @@ class Store {
 		s.values.add(value);
 		s.map.put(value,s.values.size() - 1);
 
-		System.out.println("\nAdded " + value + " to the map.");
-		System.out.println("Values: " + s.values.toString());
-		System.out.println("Map: " + s.map.toString());
+		// System.out.println("\nAdded " + value + " to the map.");
+		// System.out.println("Values: " + s.values.toString());
+		// System.out.println("Map: " + s.map.toString());
 
 	}
 
